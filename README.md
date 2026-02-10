e1c08afacbe04466cdf4f4a6639d78aad112559e
Brianna Narozny <briannanarozny@Briannas-MacBook-Air-2.local>
2026-02-09 22:27:29 -0500

    added index.html + assets/

---
+++ README.md
@@ -0,0 +1,2 @@
+# The Ocean Club of Duck
+This are the files required for The Ocean Club of Duck website. 
---
+++ script.js
@@ -0,0 +1,47 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, { threshold: 0.2 });
+
+document.querySelectorAll('.animate').forEach(el => {
+    observer.observe(el);
+});
+
+
+/* Active Navbar Link On Scroll */
+const sections = document.querySelectorAll("section, header");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 120;
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+    });
+});
+
+/* Hero Image Slider */
+const slides = document.querySelectorAll(".hero-img");
+let currentSlide = 0;
+
+function changeSlide() {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add("active");
+}
+
+setInterval(changeSlide, 4500); // change image every 4.5 seconds
+
